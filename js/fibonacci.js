1d241d503e72b236dbc9e3f7c83eaeb6b7eff208
Taras <47187659+tarashaidar@users.noreply.github.com>
2019-03-05 22:15:33 +0200

    Update fibonacci.js

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -30,3 +30,40 @@ function fibonacci(n) {
 alert( fibonacci(5) ); // 5
 
 
+
+
+//////getFibonachi///////
+function getFibonachi(n)
+{
+    if (n == 0){
+        return 0;
+    }
+    if (n == 1){
+        return 1;
+    }
+    else{
+        return getFibonachi(n - 1) + getFibonachi(n - 2);
+    }
+}
+var result = getFibonachi(8); //21 
+console.log(result); // 21
+//////getFibonachi///////
+
+
+
+
+//////getFactorial///////
+function getFactorial(n){
+    if (n === 1){
+        return 1;
+    }
+    else{
+         
+        return n * getFactorial(n - 1);
+    }
+}
+var result = getFactorial(4); 
+console.log(result); // 24
+//////getFactorial///////
+
+
