497da37ace4872e72306de1b673b94b17a0ecfdc
Taras <47187659+tarashaidar@users.noreply.github.com>
2019-03-06 16:42:48 +0200

    add class

--- js/min.js
+++ js/min.js
@@ -12,5 +12,35 @@ function min(a, b, c) {
     }
  }
 
- min(10, 12, 11) // 5  //Write digitals
+ min(10, 12, 11) // 10  //Write digitals
  min(3, 8, 4) // 3
+
+
+
+
+  ////done with classes////////////
+class Min{
+    constructor(a, b, c){
+        this.a = a; 
+        this.b = b; 
+        this.c = c; 
+    }
+
+    search_min(){
+        if (typeof this.a === 'string' || typeof this.b === 'string'|| typeof this.c === 'string') {
+            return alert('Error');
+        }
+        if (this.a < this.b && this.a < this.c) {
+            console.log(this.a);
+        } else if (this.b < this.a && this.b < this.c) {
+            console.log(this.b);
+        }
+        else{
+            console.log(this.c);
+        }
+    }
+}
+
+let min_value = new Min(1, 2, 3);
+min_value.search_min();
+////done with classes/////////////
