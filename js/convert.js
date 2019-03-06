6880eea701c1ac3e2d771872f731624e1167cad4
Taras <47187659+tarashaidar@users.noreply.github.com>
2019-03-06 16:22:01 +0200

    Update convert.js
    add class

--- js/convert.js
+++ js/convert.js
@@ -4,3 +4,22 @@ function convert(amount) {
 }
 let result = convert(10);
 console.log("Enter sum:" , result);
+//////////////////////////////////
+
+
+////done with classes////////////
+class Converter {
+	constructor (amount) {
+    this.usdExchange = 8;
+    this.amount = amount;
+  }
+ 
+  convertToUah() {
+      let uahAmount = this.amount * this.usdExchange;
+      return uahAmount;  
+  }
+}
+
+let exchangeOperation = new Converter(20);
+console.log("Enter sum:", exchangeOperation.convertToUah());
+////done with classes/////////////
