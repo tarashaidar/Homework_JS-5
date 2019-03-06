cffc617819bfe2129e6945d87e32ccd418efce02
Taras <47187659+tarashaidar@users.noreply.github.com>
2019-03-06 16:34:19 +0200

    add class

--- js/sumRange.js
+++ js/sumRange.js
@@ -14,3 +14,31 @@ function sumRange(start, end) {
 
  sumRange(2, 4) // 9  // Write a range
  sumRange(-1, 3) // 5 
+
+
+
+ ////done with classes////////////
+class Range{
+    constructor(start, end){
+        this.start = start;
+        this.end = end;
+        this.sum = 0;
+    }
+
+    sum_range(){
+        while ( this.start <= this.end ) {
+            this.sum += this.start; 
+            this.start++;
+            
+        }
+        if (this.start = this.end) {
+            console.log(this.sum)
+        }
+        return this.sum;
+    }
+}
+
+let sum_cl = new Range(2, 4);
+sum_cl.sum_range();
+////done with classes/////////////
+ 
